03d14508f2d06167b12f4dadb226aff27065556c
Igor25275 <igor_programacao25@icloud.com>
2025-05-25 14:08:58 +0000

    criaçao de pastas para organizar melhor as atividades

---
+++ atividades_1/nivel_aventureiro/modulo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// função para entrada de dados
+void entrada_de_dados(){
+
+}
+
+// função para calcular a média
+float calcular_media(){
+
+}
+
+// funçao para exibir a média
+void exibir_media(){
+
+}
+
+int main(){
+
+    entrada_de_dados();
+    calcular_media();
+    exibir_media();
+
+    return 0;
+}
