7a2c3e84578347e84fc2d6598cc42c27428f7b23
Igor25275 <igor_programacao25@icloud.com>
2025-06-01 01:14:50 +0000

    operadores relacionais

---
+++ atividades_1/nivel_mestre/operador_relacional1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 20;
+    int b = 20;
+
+    printf("a >= b: %d\n", a >= b); // maior ou igual a 
+    printf("a <= b: %d\n", a <= b); // menor ou igual a 
+    printf("a == b: %d\n", a == b); // igual a 
+    printf("a != b: %d\n", a != b); // diferente de 
+
+
+    return 0;
+}
